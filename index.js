a99203a5f2133e8b77500112b86d0668a1bdea1b
Sean Chen <seanchen11235@gmail.com>
2025-04-16 11:22:47 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+const Moralis = require("Moralis").default;
+
+require("dotenv").config({ path: ".env" });
+
+const app = express();
+const port = 5001;
+const API_KEY = process.env.API_KEY;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/fetchnfts", async (req, res) => {
+  try {
+    const { query } = req;
+
+    const response = await Moralis.EvmApi.nft.getWalletNFTs({
+      address: query.address,
+      chain: query.chain,
+    });
+
+    return res.status(200).json(response);
+  } catch (e) {
+    console.error("Server error: `${e}`");
+    return res.status(400).json();
+  }
+});
+
+Moralis.start({
+  apiKey: API_KEY,
+}).then(() => {
+  app.listen(port, () => {
+    console.log("Listening...");
+  });
+});
